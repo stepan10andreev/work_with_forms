a6dac498615da9f9a9a097cc42017b20470ccbd3
Stepan Andreev <stepan10andreev@gmail.com>
2023-07-10 11:03:01 +0300

    connect PhoneInout with store

--- src/components/ui-components/PhoneInput/PhoneInput.tsx
+++ src/components/ui-components/PhoneInput/PhoneInput.tsx
@@ -1,19 +1,30 @@
 'use client'
-import React, { FC } from 'react'
+import React, { ChangeEventHandler, FC } from 'react'
 import InputMask, { Props } from 'react-input-mask';
 import { IUIInputProps, UIInput } from '../UIInput/UIInput';
 import styles from './PhoneInput.module.scss'
+import { useAppDispatch } from '@/components/Hooks/useApp';
+import { updateUserViewFormData } from '@/store/userViewFormData';
 
 interface IPhoneInput extends Pick<IUIInputProps, 'heading' | 'placeholderText' | 'name'> {
   mask: string;
 }
 
 export const PhoneInput: FC<IPhoneInput> = ({mask, heading, placeholderText, name}) => {
+  const dispatch = useAppDispatch();
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const currentTarget = event.target;
+    console.log(currentTarget.value)
+    dispatch(updateUserViewFormData(currentTarget.name, currentTarget.value))
+  }
+
+
   return (
-    <InputMask mask={mask}>
+    <InputMask mask={mask} onChange={onChange}>
       <label className={styles.label}>
         <h2 className={styles.heading}>{heading}</h2>
-        <input className={styles.input} placeholder={placeholderText} name={name}/>
+        <input className={styles.input} placeholder={placeholderText} name={name} />
       </label>
     </InputMask>
   )
