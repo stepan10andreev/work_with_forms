c0ff72bd4c05e8c56523b9901ad05c307c14c50b
Stepan Andreev <stepan10andreev@gmail.com>
2023-07-10 10:11:03 +0300

    add email validation in UIInput

--- src/components/ui-components/UIInput/UIInput.tsx
+++ src/components/ui-components/UIInput/UIInput.tsx
@@ -26,6 +26,7 @@ export interface IUIInputProps {
 
 export const UIInput: FC<IUIInputProps> = ({ type, heading, placeholderText, name, As ='h2', formName}) => {
   const [isCyrillicText, setIsCyrillicText] = useState(true);
+  const [isCorrectEmail, setIsCorrectEmail] = useState(true);
 
   const currentValue = useAppSelector((state) => state.userViewForm[name]);
 
@@ -43,8 +44,14 @@ export const UIInput: FC<IUIInputProps> = ({ type, heading, placeholderText, nam
         } else {
           setIsCyrillicText(true)
         }
+
+        break;
       case 'email':
-        // console.log(isValidEmail(currentTarget.value))
+        if (isValidEmail(currentTarget.value)) {
+          setIsCorrectEmail(true)
+        }
+
+        break;
     }
 
     switch (formName) {
@@ -55,14 +62,29 @@ export const UIInput: FC<IUIInputProps> = ({ type, heading, placeholderText, nam
 
   const handleBlur: ChangeEventHandler<HTMLInputElement> = (event) => {
     const currentTarget = event.target;
-    currentTarget.value = getCorrectInputValue(currentTarget.value);
-    dispatch(updateUserViewFormData(currentTarget.name, currentTarget.value));
 
-    if (currentTarget.value.length > 0) {
-      dispatch(capitalizeLettersOfFullName());
+    switch (currentTarget.type) {
+      case 'text':
+        currentTarget.value = getCorrectInputValue(currentTarget.value);
+
+        dispatch(updateUserViewFormData(currentTarget.name, currentTarget.value));
+
+        if (currentTarget.value.length > 0) {
+          dispatch(capitalizeLettersOfFullName());
+        }
+
+        isCyrillic(currentTarget.value) ? setIsCyrillicText(true) : setIsCyrillicText(false);
+
+        break;
+      case 'email':
+        if (!isValidEmail(currentTarget.value)) {
+          setIsCorrectEmail(false)
+        } else {
+          setIsCorrectEmail(true)
+        }
+
+        break;
     }
-
-    isCyrillic(currentTarget.value) ? setIsCyrillicText(true) : setIsCyrillicText(false);
   }
 
   return (
@@ -70,6 +92,7 @@ export const UIInput: FC<IUIInputProps> = ({ type, heading, placeholderText, nam
       <As className={styles.heading}>{heading}</As>
       <input type={type} name={name} placeholder={placeholderText} value={currentValue} className={styles.input} onChange={onChange} onBlur={handleBlur}/>
       {!isCyrillicText && (<ErrorText errorText={'Введите только буквы кириллицы'}/>)}
+      {!isCorrectEmail && (<ErrorText errorText={'Неправильный формат email'}/>)}
     </label>
   )
 }
